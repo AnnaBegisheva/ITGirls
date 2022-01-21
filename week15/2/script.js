816c58a358a88d04410ef79824605972ef26c675
AnnaB <annabegisheva@gmail.com>
2022-01-21 20:11:11 +0300

    errors removal added

--- week15/2/script.js
+++ week15/2/script.js
@@ -10,65 +10,88 @@ function check () {
 
     if (name.value == '') {
         document.getElementById ('errorName').style.display='flex';
-        document.getElementById ('userName').style.border="1px solid #d93025";
+        name.style.border="1px solid #d93025";
         c = false;
-    } 
+    } else {
+        document.getElementById ('errorName').style.display='none';
+        name.style.removeProperty('border');
+    }
 
     if (surname.value == '') {
         document.getElementById ('errorSurname').style.display='flex';
-        document.getElementById ('userSurname').style.border="1px solid #d93025";
+        surname.style.border="1px solid #d93025";
         c = false;
-    } 
+    } else {
+        document.getElementById ('errorSurname').style.display='none';
+        surname.style.removeProperty('border');
+    }
 
     if (name.value == '' && surname.value == '') {
         document.getElementById ('errorNameSurname').style.display='flex';
-        document.getElementById ('userName').style.border="1px solid #d93025";
-        document.getElementById ('userSurname').style.border="1px solid #d93025";
+        name.style.border="1px solid #d93025";
+        surname.style.border="1px solid #d93025";
         document.getElementById('errorName').innerHTML="";
         document.getElementById('errorSurname').innerHTML="";
         c = false;
-    } 
+    } else {
+        document.getElementById ('errorNameSurname').style.display='none';
+        name.style.removeProperty('border');
+        surname.style.removeProperty('border');
+}
 
     if (email.value == '') {
         document.getElementById ('errorEmail').style.display='flex';
         document.getElementById ('loginLabel').style.display='none';
-        document.getElementById ('userEmail').style.border="1px solid #d93025";
+        email.style.border="1px solid #d93025";
         c = false;
     } else if (email.value.length <=5) {
         document.getElementById ('errorEmailLength').style.display='flex';
         document.getElementById ('loginLabel').style.display='none';
-        document.getElementById ('userEmail').style.border="1px solid #d93025";
+        email.style.border="1px solid #d93025";
         c = false;
     } else if (!isEmail(email.value)) {
         document.getElementById ('errorEmailSymbols').style.display='flex';
         document.getElementById ('loginLabel').style.display='none';
-        document.getElementById ('userEmail').style.border="1px solid #d93025";
+        email.style.border="1px solid #d93025";
         c = false;
-    } 
+    } else {
+        document.getElementById ('errorEmail').style.display='none';
+        document.getElementById ('errorEmailLength').style.display='none';
+        document.getElementById ('errorEmailSymbols').style.display='none';
+        email.style.removeProperty('border');
+}
 
     if (password.value == '') {
         document.getElementById ('errorPW').style.display='flex';
         document.getElementById ('PWLabel').style.display='none';
-        document.getElementById ('userPW').style.border="1px solid #d93025";
+        password.style.border="1px solid #d93025";
         c = false;
     } else if (password.value.length <=7) {
         document.getElementById ('errorPWLength').style.display='flex';
         document.getElementById ('PWLabel').style.display='none';
-        document.getElementById ('userPW').style.border="1px solid #d93025";
+        password.style.border="1px solid #d93025";
         c = false;
     } else if (!isPW (password.value)) {
         document.getElementById ('errorPWSymbols').style.display='flex';
         document.getElementById ('PWLabel').style.display='none';
-        document.getElementById ('userPW').style.border="1px solid #d93025";
+        password.style.border="1px solid #d93025";
         c = false;
-    } 
+    } else {
+        document.getElementById ('errorPW').style.display='none';
+        document.getElementById ('errorPWLength').style.display='none';
+        document.getElementById ('errorPWSymbols').style.display='none';
+        password.style.removeProperty('border');
+    }
 
     if (password.value !== passwordConf.value) {
         document.getElementById ('errorPWConf').style.display='flex';
         document.getElementById ('PWLabel').style.display='none';
-        document.getElementById ('userPWConf').style.border="1px solid #d93025";
+        passwordConf.style.border="1px solid #d93025";
         c = false;
-    } 
+    } else {
+        document.getElementById ('errorPWConf').style.display='none';
+        passwordConf.style.removeProperty('border');
+}
     
     if (c == true) {
         alert (`Hello, ${name.value}`);
