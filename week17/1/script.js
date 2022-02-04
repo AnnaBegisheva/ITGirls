e1acaf3441d40a6389081924a29cf9cae3e5ae49
AnnaBegisheva <annabegisheva@gmail.com>
2022-02-04 20:31:49 +0300

    1.17 updated

--- week17/1/script.js
+++ week17/1/script.js
@@ -1,11 +1,20 @@
 function getName() {
      let userName = document.getElementById("userFullName").value.toLowerCase().replace(/ +/g, ' ').trim();
      let array = userName.split(" ");
-     const newArray = [];
-     for (let a of array) {
+     const newArray = ['', '', ''];
+     for (let i = 0; i < array.length; i++) {
+          let a = array[i];
+          newName = a[0].toUpperCase() + a.slice(1);
+          newArray[i] = newName;
+     }
+
+
+     document.getElementById("result").innerHTML = `<div>Фамилия: ${newArray[0]}</div> <div>Имя: ${newArray[1]}</div> <div>Отчество: ${newArray[2]}</div>`
+}
+
+/*
+for (let a of array) {
           newName = a[0].toUpperCase() + a.slice(1);
           newArray.push(newName);
      }
-
-     document.getElementById("result").innerHTML = `<div>Фамилия: ${newArray[0]},</div> <div>Имя: ${newArray[1]},</div> <div>Отчество: ${newArray[2]}</div>`
-}
+     */
