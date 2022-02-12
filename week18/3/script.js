b4572828f85a959385f7073de2f4790f6753bebd
AnnaBegisheva <annabegisheva@gmail.com>
2022-02-12 23:01:26 +0300

    comments first commit

--- week18/3/script.js
+++ week18/3/script.js
@@ -1,24 +1,28 @@
-const comments = [];
-
 function checkSpam() {
-    let str = document.getElementById("newComment").value;
-    str = str.replace(/viagra/ig, "***")
-    str = str.replace(/xxx/ig, "***")
+    let comment = document.getElementById("newComment").value;
+    comment = comment.replace(/viagra/ig, "***")
+    comment = comment.replace(/xxx/ig, "***")
 
-    comments.push(str);
-    generateComment()
+    generateComment(comment)
 }
 
-function generateComment() {
-    let str = document.getElementById("newComment").value;
+function generateComment(comment) {
     let name = document.getElementById("author").value;
 
-    if (str != '' && name !='') {
+    if (comment != '' && name != '') {
+        let wrapper = document.createElement('div');
         let commentBox = document.createElement('div');
-        for (let comment of comments) {
-            commentBox.innerHTML = `${name}: ${comment}`
-        }
-        message.append(commentBox);
+        let avatarBox = document.createElement('div');
+        let path = document.getElementById('avatarImg');
+
+        commentBox.innerHTML = `<strong>${name}:</strong> <br> ${comment}`;
+        
+        message.append(wrapper);
+        wrapper.append(avatarBox);
+        wrapper.append(commentBox);
+        avatarBox.style.background = window.getComputedStyle(path).background;
+        avatarBox.style.backgroundSize = 'cover';
+        avatarBox.style.backgroundRepeat = 'no-repeat';
     }
     checkAuthor()
 }
@@ -29,7 +33,6 @@ document.addEventListener("DOMContentLoaded",
         if (name != null) {
             document.getElementById('author').value = name;
         }
-        generateComment();
     });
 
 function checkAuthor() {
@@ -40,3 +43,10 @@ function checkAuthor() {
     }
 };
 
+function changeAvatar () {
+    let avatarPath = document.getElementById("avatarURL").value;
+    let avatarImg = document.getElementById("avatarImg");
+    avatarImg.style.background = `url(${avatarPath})`;
+    avatarImg.style.backgroundSize = 'cover';
+    avatarImg.style.backgroundRepeat = 'no-repeat';
+}
