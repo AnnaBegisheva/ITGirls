5cd4bd9733edf5b8d0b2e43d233e433613e58ac4
AnnaBegisheva <annabegisheva@gmail.com>
2022-03-12 18:14:43 +0300

    gifs done

--- week21/gifs/script.js
+++ week21/gifs/script.js
@@ -1,15 +1,14 @@
 let searchGifs = (i) => {
-let searchRequest = document.querySelector('.search').value;
+    let searchRequest = document.querySelector('.search').value;
 
-    fetch('https://api.giphy.com/v1/gifs/search?api_key=Y9QDRtJqJgbVios4mOhHlWq7az3YrqTJ&q=' + searchRequest + '&limit=5&offset=0&rating=g&lang=en', {
-        mode: 'no-cors'
-    })
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=Y9QDRtJqJgbVios4mOhHlWq7az3YrqTJ&q=' + searchRequest + '&limit=5&offset=0&rating=g&lang=en'
+        )
         .then(response => response.json())
         .then(gif => {
             console.log(gif);
-            for(let i = 0; i < gif.data.length; i++) {
+            for (let i = 0; i < gif.data.length; i++) {
                 let box = document.createElement("img");
-                box.src = gif.data[i].url;
+                box.src = gif.data[i].images.original.url;
                 let container = document.querySelector('.container');
                 container.append(box);
             }
@@ -17,7 +16,14 @@ let searchRequest = document.querySelector('.search').value;
         .catch(error => console.log(error));
 }
 
-button.addEventListener('click', function(){
+let button = document.querySelector(".button")
+button.onclick = function (e) {
+    e.preventDefault();
+    clearContainer();
     searchGifs();
+    document.querySelector('.search').value = '';
 }
-);
+
+let clearContainer = () => {
+    document.querySelector('.container').innerHTML = '';
+}
