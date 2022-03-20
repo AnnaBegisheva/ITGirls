794cc5a702da0afd5649db186f18e137375534dc
AnnaBegisheva <annabegisheva@gmail.com>
2022-03-20 14:19:56 +0300

    tasks layout

--- week22/dashboard/bundle.js
+++ week22/dashboard/bundle.js
@@ -96,6 +96,9 @@ const barChart = new Chart(ctx, {
             backgroundColor: [
                 'rgba(139, 196, 255, 0.5)'
             ],
+            hoverBackgroundColor: [
+                'rgb(139, 196, 255)'
+            ],
             borderColor: [
                 'rgb(46, 88, 255)'
             ],
@@ -19111,40 +19114,4 @@ const picker = datepicker('#datepicker', {
     // noWeekends: true, // Saturday's and Sunday's will be unselectable.
   });
 
-
-  const tasksDatepicker = require('js-datepicker');
-  const tasksPicker = tasksDatepicker('#tasksDatepicker', {
-      // Event callbacks.
-      onSelect: instance => {
-        // Show which date was selected.
-        console.log(instance.dateSelected)
-      },
-      onMonthChange: instance => {
-        // Show the month of the selected date.
-        console.log(instance.currentMonthName)
-      },
-    
-      // Customizations.
-      formatter: (input, date, instance) => {
-        // This will display the date as `1/1/2019`.
-        input.value = date.toDateString()
-      },
-      position: 'br', // Top right.
-      startDay: 1, // Calendar week starts on a Monday.
-      customDays: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-      overlayButton: 'Go!',
-      overlayPlaceholder: 'Enter a year',
-    
-      // Settings.
-      alwaysShow: true, // Never hide the calendar.
-      dateSelected: new Date(), // Today is selected.
-      maxDate: new Date(2099, 0, 1), // Jan 1st, 2099.
-      minDate: new Date(2016, 5, 1), // June 1st, 2016.
-      startDate: new Date(), // This month.
-      showAllDates: true, // Numbers for leading & trailing days outside the current month will show.
-    
-      // Disabling things.
-      // noWeekends: true, // Saturday's and Sunday's will be unselectable.
-    });
-  
 },{"js-datepicker":3,"moment":4}]},{},[1,5]);
---
+++ week22/dashboard/tasks/tasks.js
@@ -0,0 +1,39 @@
+const datepicker = require('js-datepicker');
+const picker = datepicker('#datepicker', {
+    // Event callbacks.
+    onSelect: instance => {
+      // Show which date was selected.
+      console.log(instance.dateSelected)
+    },
+    onMonthChange: instance => {
+      // Show the month of the selected date.
+      console.log(instance.currentMonthName)
+    },
+  
+    // Customizations.
+    formatter: (input, date, instance) => {
+      // This will display the date as `1/1/2019`.
+      input.value = date.toDateString()
+    },
+    position: 'br', // Top right.
+    startDay: 1, // Calendar week starts on a Monday.
+    customDays: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+    overlayButton: 'Go!',
+    overlayPlaceholder: 'Enter a year',
+  
+    // Settings.
+    alwaysShow: true, // Never hide the calendar.
+    dateSelected: new Date(), // Today is selected.
+    maxDate: new Date(2099, 0, 1), // Jan 1st, 2099.
+    minDate: new Date(2016, 5, 1), // June 1st, 2016.
+    startDate: new Date(), // This month.
+    showAllDates: true, // Numbers for leading & trailing days outside the current month will show.
+  
+    // Disabling things.
+    // noWeekends: true, // Saturday's and Sunday's will be unselectable.
+  });
+
+  const moment = require ('moment');
+let pickedDate = moment().format("dddd MMMM Do");
+console.log(pickedDate);
+document.querySelector(".pickedDate").innerHTML = pickedDate;
